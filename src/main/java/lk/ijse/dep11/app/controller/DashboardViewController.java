bf0407a68f83533f30ca087c9f5592e8bdb821ad
Tharindu Nuwan Madhushanka <tharindunm94@gmail.com>
2023-10-17 20:41:21 +0530

    Design DashBord in Scene Builder

--- src/main/java/lk/ijse/dep11/app/controller/DashboardViewController.java
+++ src/main/java/lk/ijse/dep11/app/controller/DashboardViewController.java
@@ -1,4 +1,34 @@
 package lk.ijse.dep11.app.controller;
 
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+
 public class DashboardViewController {
+    public AnchorPane root;
+    public ImageView imgCustomer;
+    public ImageView imgItem;
+    public ImageView imgOrder;
+    public ImageView imgSearch;
+    public Label lblTitle;
+    public Label lblDescription;
+
+    public void imgCustomerOnMouseClicked(MouseEvent mouseEvent) {
+    }
+
+    public void imgItemOnMouseClicked(MouseEvent mouseEvent) {
+    }
+
+    public void imgOrderOnMouseClicked(MouseEvent mouseEvent) {
+    }
+
+    public void imgSearchOnMouseClicked(MouseEvent mouseEvent) {
+    }
+
+    public void playMouseEnterAnimation(MouseEvent mouseEvent) {
+    }
+
+    public void playMouseExitAnimation(MouseEvent mouseEvent) {
+    }
 }
