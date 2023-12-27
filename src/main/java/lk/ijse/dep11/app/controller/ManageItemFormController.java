93a405341d3b83e250852035c4c3567f9c330dac
Tharindu Nuwan Madhushanka <tharindunm94@gmail.com>
2023-12-27 21:44:12 +0530

    Finish implement ManageItemController

--- src/main/java/lk/ijse/dep11/app/controller/ManageItemFormController.java
+++ src/main/java/lk/ijse/dep11/app/controller/ManageItemFormController.java
@@ -3,16 +3,26 @@ package lk.ijse.dep11.app.controller;
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXTextField;
 import javafx.animation.FadeTransition;
+import javafx.application.Platform;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 import javafx.util.Duration;
+import lk.ijse.dep11.app.db.ItemDataAccess;
+import lk.ijse.dep11.app.db.OrderDataAccess;
+import lk.ijse.dep11.app.tm.Item;
 
 import java.io.IOException;
+import java.math.BigDecimal;
+import java.sql.SQLException;
 
 public class ManageItemFormController {
     public AnchorPane root;
@@ -22,7 +32,7 @@ public class ManageItemFormController {
     public JFXTextField txtQtyOnHand;
     public JFXButton btnSave;
     public JFXButton btnDelete;
-    public TableView tblItems;
+    public TableView<Item> tblItems;
     public JFXTextField txtUnitPrice;
 
     public void initialize() {
@@ -30,6 +40,38 @@ public class ManageItemFormController {
         fadeIn.setFromValue(0.0);
         fadeIn.setToValue(1.0);
         fadeIn.play();
+
+        tblItems.getColumns().get(0).setCellValueFactory(new PropertyValueFactory<>("code"));
+        tblItems.getColumns().get(1).setCellValueFactory(new PropertyValueFactory<>("description"));
+        tblItems.getColumns().get(2).setCellValueFactory(new PropertyValueFactory<>("qty"));
+        tblItems.getColumns().get(3).setCellValueFactory(new PropertyValueFactory<>("unitPrice"));
+        btnDelete.setDisable(true);
+        btnSave.setDefaultButton(true);
+
+        try {
+            tblItems.getItems().addAll(ItemDataAccess.getAllItems());
+        } catch (SQLException e) {
+            e.printStackTrace();
+            new Alert(Alert.AlertType.ERROR,
+                    "Failed to load items, try again later!").show();
+        }
+        Platform.runLater(txtCode::requestFocus);
+
+        tblItems.getSelectionModel().selectedItemProperty().addListener((ov, prev, cur) -> {
+            if (cur == null) {
+                btnSave.setText("SAVE");
+                btnDelete.setDisable(true);
+                txtCode.setDisable(false);
+            } else {
+                btnSave.setText("UPDATE");
+                btnDelete.setDisable(false);
+                txtCode.setText(cur.getCode());
+                txtCode.setDisable(true);
+                txtDescription.setText(cur.getDescription());
+                txtQtyOnHand.setText(cur.getQty() + "");
+                txtUnitPrice.setText(cur.getUnitPrice() + "");
+            }
+        });
     }
 
     public void navigateToHome(MouseEvent mouseEvent) throws IOException {
@@ -44,11 +86,94 @@ public class ManageItemFormController {
     }
 
     public void btnAddNew_OnAction(ActionEvent actionEvent) {
+        for (TextField textField : new TextField[]{txtCode, txtDescription, txtUnitPrice, txtQtyOnHand}) {
+            textField.clear();
+        }
+        txtCode.requestFocus();
+        tblItems.getSelectionModel().clearSelection();
     }
 
     public void btnSave_OnAction(ActionEvent actionEvent) {
+        if (!isDataValid()) return;
+        try {
+            Item item = new Item(txtCode.getText().strip(), txtDescription.getText().strip(),
+                    Integer.parseInt(txtQtyOnHand.getText()), new BigDecimal(txtUnitPrice.getText()).setScale(2));
+
+            if (btnSave.getText().equals("SAVE")) {
+                if (ItemDataAccess.existItem(item.getCode())) {
+                    new Alert(Alert.AlertType.ERROR, "Item code already exist!").show();
+                    txtCode.requestFocus();
+                    txtCode.selectAll();
+                    return;
+                }
+                ItemDataAccess.saveItem(item);
+                tblItems.getItems().add(item);
+                new Alert(Alert.AlertType.INFORMATION, "Item saved successfully!").show();
+            }else {
+                ItemDataAccess.updateItem(item);
+                ObservableList<Item> itemsList = tblItems.getItems();
+                Item selectedItem = tblItems.getSelectionModel().getSelectedItem();
+                itemsList.set(itemsList.indexOf(selectedItem), item);
+                new Alert(Alert.AlertType.INFORMATION, "Item updated successfully!").show();
+                tblItems.refresh();
+            }
+            btnAddNew.fire();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            new Alert(Alert.AlertType.ERROR, "Failed to save the item, try again!").show();
+        }
     }
 
     public void btnDelete_OnAction(ActionEvent actionEvent) {
+        Item selectedItem = tblItems.getSelectionModel().getSelectedItem();
+        try {
+            if (OrderDataAccess.existOrderByItemCode(selectedItem.getCode())) {
+                new Alert(Alert.AlertType.ERROR, "Failed to delete, the item already associated with an order!")
+                        .show();
+            } else {
+                ItemDataAccess.deleteItem(selectedItem.getCode());
+                tblItems.getItems().remove(selectedItem);
+                if (tblItems.getItems().isEmpty()) btnAddNew.fire();
+                new Alert(Alert.AlertType.INFORMATION, "Item deleted successfully!").show();
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            new Alert(Alert.AlertType.ERROR, "Failed to delete the item, try again!").show();
+        }
+    }
+
+    private boolean isDataValid() {
+        String code = txtCode.getText().strip();
+        String description = txtDescription.getText().strip();
+        String qty = txtQtyOnHand.getText().strip();
+        String unitPrice = txtUnitPrice.getText().strip();
+
+        if (!code.matches("\\d{4,}")) {
+            txtCode.requestFocus();
+            txtCode.selectAll();
+            return false;
+        } else if (!description.matches("^[A-Za-z0-9 ]{4,}$")) {
+            txtDescription.requestFocus();
+            txtDescription.selectAll();
+            return false;
+        } else if (!qty.matches("\\d+") || Integer.parseInt(qty) <= 0) {
+            txtQtyOnHand.requestFocus();
+            txtQtyOnHand.selectAll();
+            return false;
+        } else if (!isPrice(unitPrice)) {
+            txtUnitPrice.requestFocus();
+            txtUnitPrice.selectAll();
+            return false;
+        }
+        return true;
+    }
+
+    private boolean isPrice(String unitPrice) {
+        try {
+            double price = Double.parseDouble(unitPrice);
+            return price > 0;
+        } catch (NumberFormatException e) {
+            return false;
+        }
     }
 }
--- src/main/java/lk/ijse/dep11/app/controller/PlaceOrderFormController.java
+++ src/main/java/lk/ijse/dep11/app/controller/PlaceOrderFormController.java
@@ -4,17 +4,30 @@ import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXComboBox;
 import com.jfoenix.controls.JFXTextField;
 import javafx.animation.FadeTransition;
+import javafx.application.Platform;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.Label;
 import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 import javafx.util.Duration;
+import lk.ijse.dep11.app.db.CustomerDataAccess;
+import lk.ijse.dep11.app.db.ItemDataAccess;
+import lk.ijse.dep11.app.db.OrderDataAccess;
+import lk.ijse.dep11.app.tm.Customer;
+import lk.ijse.dep11.app.tm.Item;
+import lk.ijse.dep11.app.tm.OrderItem;
 
 import java.io.IOException;
+import java.sql.SQLException;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 public class PlaceOrderFormController {
     public AnchorPane root;
@@ -22,9 +35,9 @@ public class PlaceOrderFormController {
     public JFXTextField txtDescription;
     public JFXTextField txtQtyOnHand;
     public JFXButton btnSave;
-    public TableView tblOrderDetails;
-    public JFXComboBox cmbCustomerId;
-    public JFXComboBox cmbItemCode;
+    public TableView<OrderItem> tblOrderDetails;
+    public JFXComboBox<Customer> cmbCustomerId;
+    public JFXComboBox<Item> cmbItemCode;
     public JFXTextField txtUnitPrice;
     public JFXTextField txtQty;
     public Label lblId;
@@ -32,11 +45,95 @@ public class PlaceOrderFormController {
     public Label lblTotal;
     public JFXButton btnPlaceOrder;
 
-    public void initialize() {
+    public void initialize() throws IOException {
         FadeTransition fadeIn = new FadeTransition(Duration.millis(1000), root);
         fadeIn.setFromValue(0.0);
         fadeIn.setToValue(1.0);
         fadeIn.play();
+
+        String[] cols = {"code", "description", "qty", "unitPrice", "total", "btnDelete"};
+        for (int i = 0; i < cols.length; i++) {
+            tblOrderDetails.getColumns().get(i)
+                    .setCellValueFactory(new PropertyValueFactory<>(cols[i]));
+        }
+
+        lblDate.setText(LocalDate.now().format(DateTimeFormatter.ofPattern("dd-MM-yyyy")));
+        newOrder();
+
+        cmbCustomerId.getSelectionModel().selectedItemProperty().addListener((ov, prev, cur)-> {
+            enablePlaceOrderButton();
+            if (cur != null) {
+                txtCustomerName.setText(cur.getName());
+                txtCustomerName.setDisable(false);
+                txtCustomerName.setEditable(false);
+            } else {
+                txtCustomerName.clear();
+                txtCustomerName.setDisable(true);
+            }
+        });
+
+        cmbItemCode.getSelectionModel().selectedItemProperty().addListener((ov, prev, cur)-> {
+            if (cur != null) {
+                txtDescription.setText(cur.getDescription());
+                txtQtyOnHand.setText(cur.getQty() + "");
+                txtUnitPrice.setText(cur.getUnitPrice().toString());
+
+                for (TextField textField : new TextField[]{txtDescription, txtQtyOnHand, txtUnitPrice}) {
+                    textField.setDisable(false);
+                    textField.setEditable(false);
+                }
+                txtQty.setDisable(cur.getQty() == 0);
+            } else {
+                for (TextField textField : new TextField[]{txtDescription, txtQtyOnHand, txtUnitPrice, txtQty}) {
+                    textField.setDisable(true);
+                    textField.clear();
+                }
+            }
+        });
+        txtQty.textProperty().addListener((ov, prev, cur)-> {
+            Item selectedItem = cmbItemCode.getSelectionModel().getSelectedItem();
+            btnSave.setDisable(true);
+            if (cur.matches("\\d+")) {
+                if (Integer.parseInt(cur) <= selectedItem.getQty() && Integer.parseInt(cur) > 0) {
+                    btnSave.setDisable(false);
+                }
+            }
+        });
+    }
+
+    private void enablePlaceOrderButton() {
+    }
+
+    private void newOrder() throws IOException {
+        for (TextField textField : new TextField[]{txtCustomerName, txtDescription, txtQtyOnHand, txtUnitPrice, txtQty}) {
+            textField.setDisable(true);
+            textField.clear();
+        }
+        tblOrderDetails.getItems().clear();
+        lblTotal.setText("00.00");
+        btnSave.setDisable(true);
+        btnPlaceOrder.setDisable(true);
+        cmbCustomerId.getSelectionModel().clearSelection();
+        cmbItemCode.getSelectionModel().clearSelection();
+        try {
+            cmbCustomerId.getItems().clear();
+            cmbCustomerId.getItems().addAll(CustomerDataAccess.getAllCustomers());
+            cmbItemCode.getItems().clear();
+            cmbItemCode.getItems().addAll(ItemDataAccess.getAllItems());
+            String lastOrderId = OrderDataAccess.getLastOrderId();
+            if (lastOrderId == null) {
+                lblId.setText("OD001");
+            } else {
+                int newOrderId = Integer.parseInt(lastOrderId.substring(2)) + 1;
+                lblId.setText(String.format("OD%03", newOrderId));
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            new Alert(Alert.AlertType.ERROR, "Failed to establish database connection,  try later!")
+                    .show();
+            navigateToHome(null);
+        }
+        Platform.runLater(cmbCustomerId::requestFocus);
     }
 
     public void btnAdd_OnAction(ActionEvent actionEvent) {
